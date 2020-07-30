1bca0e84963568c39ffe2c6ff4ab680e25eb0eba
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2020-07-30 22:54:49 +0300

    relocate files

---
+++ src/B/B_492.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class B_492 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n, l;
+        n=Integer.parseInt(scanner.next());
+        l=Integer.parseInt(scanner.next());
+        for (int i = 0; i < n; i++) {
+            
+        }
+        scanner.close();
+    }
+}
